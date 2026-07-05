a431db97c5d85aaea2ac0d2171872921d25c0bbc
Swamp <swapnaniltripathy2006@gmail.com>
2026-07-05 14:57:02 +0530

    Add: majority-element-ii - submission-2

---
+++ Data Structures & Algorithms/majority-element-ii/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+
+        if (nums.length == 0) return ans;
+        Arrays.sort(nums);
+        int n = nums.length;
+        int count = 1;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) count++;
+            else {
+                if (count > n / 3) {
+                    ans.add(nums[i]);
+                }
+                count = 1;
+            }
+        }
+        if (count > n / 3) ans.add(nums[n - 1]);
+        return ans;
+    }
+}
