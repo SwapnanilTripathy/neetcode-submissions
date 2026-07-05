3df216fa74d2ee754723b6d7f5e97495e8bd7b72
Swamp <swapnaniltripathy2006@gmail.com>
2026-07-05 19:00:15 +0530

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long nCr(int n, int r) {
+        if (r > n) return 0;
+        if (r == 0 || r == n) return 1;
+        r = Math.min(r, n - r);
+        long res = 1;
+        for (int i = 1; i <= r; i++) {
+            res = res * (n - r + i) / i;
+        }
+        return res;
+    }
+    public int climbStairs(int n) {
+        int ans = 0;
+        for (int two = 0; two <= n / 2; two++) {
+            ans += nCr(n - two, two);
+        }
+        return ans;
+    }
+}
